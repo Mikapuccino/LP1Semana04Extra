02f92ac85fe65182f9ff0b0c3efa95c01405ec8b
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-22 18:59:51 +0000

    Made a local change

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace testrepo
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Local change, nothing will go wrong");
             SomeFunction();
         }
 
